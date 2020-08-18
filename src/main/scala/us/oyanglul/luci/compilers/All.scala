221ae5e415d8082eac8edde97874047d52cfd67e
Jichao Ouyang <oyanglulu@gmail.com>
2020-08-18 22:24:31 +1000

    fix unit test

--- src/main/scala/us/oyanglul/luci/compilers/All.scala
+++ src/main/scala/us/oyanglul/luci/compilers/All.scala
@@ -4,7 +4,6 @@ package compilers
 trait All[E[_]]
     extends GenericCompiler[E]
     with WriterCompiler[E]
-    with Http4sClientCompiler[E]
     with ReaderCompiler[E]
     with StateCompiler[E]
     with IdCompiler[E]
